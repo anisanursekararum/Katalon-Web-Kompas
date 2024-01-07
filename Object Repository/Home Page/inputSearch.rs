c4bcc07cea30c7dabedee94dabcc035fc0e597d9
anisanursekararum <anisanursekararum@gmail.com>
2024-01-07 13:54:26 +0700

    add object repo search

---
+++ Object Repository/Home Page/inputSearch.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>inputSearch</name>
+   <tag></tag>
+   <elementGuidId>6a59f429-3f15-4e54-8e0c-b173a1bd870a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id='search' and @type='text']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
